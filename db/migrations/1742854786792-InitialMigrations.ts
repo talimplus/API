58d5d5adaf835055da9bc600189e3ec974249093
nematoff98 <nematoff98@gmail.com>
2025-05-11 11:23:32 +0500

    package change

--- db/migrations/1742854786792-InitialMigrations.ts
+++ db/migrations/1742854786792-InitialMigrations.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialMigration1742851681379 implements MigrationInterface {
-    name = 'InitialMigration1742851681379'
+export class InitialMigrations1742854786792 implements MigrationInterface {
+    name = 'InitialMigrations1742854786792'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "plans" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "price" numeric(10,2) NOT NULL, "maxCenters" integer, "maxUsers" integer, "maxStudents" integer, "description" text, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_253d25dae4c94ee913bc5ec4850" UNIQUE ("name"), CONSTRAINT "PK_3720521a81c7c24fe9b7202ba61" PRIMARY KEY ("id"))`);
@@ -15,9 +15,9 @@ export class InitialMigration1742851681379 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "groups" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "subjectId" integer, "centerId" integer, "teacherId" integer, CONSTRAINT "PK_659d1483316afb28afd3a90646e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."students_status_enum" AS ENUM('yangi', 'oqimagan', 'oqiyapti', 'oqishni toxtatgan')`);
         await queryRunner.query(`CREATE TABLE "students" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "phone" character varying NOT NULL, "birthDate" date NOT NULL, "status" "public"."students_status_enum" NOT NULL DEFAULT 'yangi', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "centerId" integer, "groupId" integer, CONSTRAINT "UQ_317b86154bca256bdf5432f134c" UNIQUE ("phone"), CONSTRAINT "PK_7d7f07271ad4ce999880713f05e" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "lessons" ("id" SERIAL NOT NULL, "lessonDate" date NOT NULL, "startTime" TIME NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "groupId" integer, CONSTRAINT "PK_9b9a8d455cac672d262d7275730" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TYPE "public"."payments_status_enum" AS ENUM('tolandi', 'qisman tolandi', 'tolanmadi')`);
         await queryRunner.query(`CREATE TABLE "payments" ("id" SERIAL NOT NULL, "amount" numeric NOT NULL, "status" "public"."payments_status_enum" NOT NULL DEFAULT 'tolanmadi', "paymentDate" TIMESTAMP NOT NULL DEFAULT now(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "centerId" integer, "studentId" integer, CONSTRAINT "PK_197ab7af18c93fbb0c9b28b4a59" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "lessons" ("id" SERIAL NOT NULL, "lessonDate" date NOT NULL, "startTime" TIME NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "groupId" integer, CONSTRAINT "PK_9b9a8d455cac672d262d7275730" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "expenses" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "amount" numeric(10,2) NOT NULL, "expenseDate" TIMESTAMP NOT NULL DEFAULT now(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "centerId" integer, CONSTRAINT "PK_94c3ceb17e3140abc9282c20610" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "subscriptions" ADD CONSTRAINT "FK_a7a84c705f3e8e4fbd497cfb119" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "subscriptions" ADD CONSTRAINT "FK_7536cba909dd7584a4640cad7d5" FOREIGN KEY ("planId") REFERENCES "plans"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
@@ -30,17 +30,17 @@ export class InitialMigration1742851681379 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "groups" ADD CONSTRAINT "FK_e63173ac43b478c2fc0cc20ac39" FOREIGN KEY ("teacherId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "students" ADD CONSTRAINT "FK_9b486888f383188d0d70dd106b1" FOREIGN KEY ("centerId") REFERENCES "centers"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "students" ADD CONSTRAINT "FK_cc296de36cadf8de53b9cd60aa7" FOREIGN KEY ("groupId") REFERENCES "groups"("id") ON DELETE SET NULL ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "lessons" ADD CONSTRAINT "FK_4261adc75cbfa91fadc4fe297ec" FOREIGN KEY ("groupId") REFERENCES "groups"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_c78ec2f5492035120c394625663" FOREIGN KEY ("centerId") REFERENCES "centers"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_b2731e10aef7f886a08c552290e" FOREIGN KEY ("studentId") REFERENCES "students"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "lessons" ADD CONSTRAINT "FK_4261adc75cbfa91fadc4fe297ec" FOREIGN KEY ("groupId") REFERENCES "groups"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "expenses" ADD CONSTRAINT "FK_71fcf0da7025faa3c11b0071076" FOREIGN KEY ("centerId") REFERENCES "centers"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "expenses" DROP CONSTRAINT "FK_71fcf0da7025faa3c11b0071076"`);
+        await queryRunner.query(`ALTER TABLE "lessons" DROP CONSTRAINT "FK_4261adc75cbfa91fadc4fe297ec"`);
         await queryRunner.query(`ALTER TABLE "payments" DROP CONSTRAINT "FK_b2731e10aef7f886a08c552290e"`);
         await queryRunner.query(`ALTER TABLE "payments" DROP CONSTRAINT "FK_c78ec2f5492035120c394625663"`);
-        await queryRunner.query(`ALTER TABLE "lessons" DROP CONSTRAINT "FK_4261adc75cbfa91fadc4fe297ec"`);
         await queryRunner.query(`ALTER TABLE "students" DROP CONSTRAINT "FK_cc296de36cadf8de53b9cd60aa7"`);
         await queryRunner.query(`ALTER TABLE "students" DROP CONSTRAINT "FK_9b486888f383188d0d70dd106b1"`);
         await queryRunner.query(`ALTER TABLE "groups" DROP CONSTRAINT "FK_e63173ac43b478c2fc0cc20ac39"`);
@@ -53,9 +53,9 @@ export class InitialMigration1742851681379 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "subscriptions" DROP CONSTRAINT "FK_7536cba909dd7584a4640cad7d5"`);
         await queryRunner.query(`ALTER TABLE "subscriptions" DROP CONSTRAINT "FK_a7a84c705f3e8e4fbd497cfb119"`);
         await queryRunner.query(`DROP TABLE "expenses"`);
+        await queryRunner.query(`DROP TABLE "lessons"`);
         await queryRunner.query(`DROP TABLE "payments"`);
         await queryRunner.query(`DROP TYPE "public"."payments_status_enum"`);
-        await queryRunner.query(`DROP TABLE "lessons"`);
         await queryRunner.query(`DROP TABLE "students"`);
         await queryRunner.query(`DROP TYPE "public"."students_status_enum"`);
         await queryRunner.query(`DROP TABLE "groups"`);
