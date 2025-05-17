01152bbbfcd8f283419abab9bc2a1674c278925b
nematoff98 <nematoff98>
2025-05-17 17:38:48 +0500

    change migration file

--- db/migrations/1747172713232-NewMigrations.ts
+++ db/migrations/1747172713232-NewMigrations.ts
@@ -1,32 +1,75 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NewMigrations1747172713232 implements MigrationInterface {
-    name = 'NewMigrations1747172713232'
+  name = 'NewMigrations1747172713232';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ADD "studentId" integer`);
-        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_42dc3c1fa59ce4a36a19cff2721" UNIQUE ("studentId")`);
-        await queryRunner.query(`ALTER TABLE "students" DROP CONSTRAINT "UQ_317b86154bca256bdf5432f134c"`);
-        await queryRunner.query(`ALTER TYPE "public"."users_role_enum" RENAME TO "users_role_enum_old"`);
-        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('super_admin', 'admin', 'teacher', 'manager', 'other', 'student')`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "role" TYPE "public"."users_role_enum" USING "role"::"text"::"public"."users_role_enum"`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'admin'`);
-        await queryRunner.query(`DROP TYPE "public"."users_role_enum_old"`);
-        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "FK_42dc3c1fa59ce4a36a19cff2721" FOREIGN KEY ("studentId") REFERENCES "students"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DO $$
+      BEGIN
+        IF EXISTS (
+          SELECT 1 FROM information_schema.table_constraints 
+          WHERE constraint_name = 'UQ_42dc3c1fa59ce4a36a19cff2721'
+        ) THEN
+          ALTER TABLE "users" DROP CONSTRAINT "UQ_42dc3c1fa59ce4a36a19cff2721";
+        END IF;
+      END;
+      $$;
+    `);
+    await queryRunner.query(`ALTER TABLE "users" ADD "studentId" integer`);
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD CONSTRAINT "UQ_42dc3c1fa59ce4a36a19cff2721" UNIQUE ("studentId")`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "students" DROP CONSTRAINT "UQ_317b86154bca256bdf5432f134c"`,
+    );
+    await queryRunner.query(
+      `ALTER TYPE "public"."users_role_enum" RENAME TO "users_role_enum_old"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."users_role_enum" AS ENUM('super_admin', 'admin', 'teacher', 'manager', 'other', 'student')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" TYPE "public"."users_role_enum" USING "role"::"text"::"public"."users_role_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'admin'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."users_role_enum_old"`);
+    await queryRunner.query(
+      `ALTER TABLE "users" ADD CONSTRAINT "FK_42dc3c1fa59ce4a36a19cff2721" FOREIGN KEY ("studentId") REFERENCES "students"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "FK_42dc3c1fa59ce4a36a19cff2721"`);
-        await queryRunner.query(`CREATE TYPE "public"."users_role_enum_old" AS ENUM('super_admin', 'admin', 'teacher', 'manager', 'other')`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "role" TYPE "public"."users_role_enum_old" USING "role"::"text"::"public"."users_role_enum_old"`);
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'admin'`);
-        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
-        await queryRunner.query(`ALTER TYPE "public"."users_role_enum_old" RENAME TO "users_role_enum"`);
-        await queryRunner.query(`ALTER TABLE "students" ADD CONSTRAINT "UQ_317b86154bca256bdf5432f134c" UNIQUE ("phone")`);
-        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_42dc3c1fa59ce4a36a19cff2721"`);
-        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "studentId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "users" DROP CONSTRAINT "FK_42dc3c1fa59ce4a36a19cff2721"`,
+    );
+    await queryRunner.query(
+      `CREATE TYPE "public"."users_role_enum_old" AS ENUM('super_admin', 'admin', 'teacher', 'manager', 'other')`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" DROP DEFAULT`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" TYPE "public"."users_role_enum_old" USING "role"::"text"::"public"."users_role_enum_old"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'admin'`,
+    );
+    await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+    await queryRunner.query(
+      `ALTER TYPE "public"."users_role_enum_old" RENAME TO "users_role_enum"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "students" ADD CONSTRAINT "UQ_317b86154bca256bdf5432f134c" UNIQUE ("phone")`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "users" DROP CONSTRAINT "UQ_42dc3c1fa59ce4a36a19cff2721"`,
+    );
+    await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "studentId"`);
+  }
 }
