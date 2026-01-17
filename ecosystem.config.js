341cc4886371597de741b474779a8af224db52e4
Abdurahmon <nematoff98@gmail.com>
2026-01-17 18:59:40 +0500

    changed ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,16 +6,6 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3004,
-        DB_HOST: 'localhost',
-        DB_PORT: 5432,
-        DB_USER: 'talimplus',
-        DB_PASS: 'nematov19980524',
-        DB_NAME: 'talimplus',
-        JWT_SECRET: 'talimplus_secret',
-      },
     },
   ],
 };
